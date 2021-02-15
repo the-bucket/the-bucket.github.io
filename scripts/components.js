c2b0a9390f7f4027cc8fd1fde4c6d7a49be05fc6
ty <fqdingsky@gmail.com>
2021-02-15 15:16:23 +0800

    initial commit

---
+++ scripts/components.js
@@ -0,0 +1,62 @@
+class HeaderPart extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <header>
+      <div class="header">
+        <h1>the bucket.</h1>
+      </div>
+
+    </header>
+    <nav id="navbar">
+
+      <div class="highlight" id="highlight">
+      </div>
+
+      <ul id="test">
+        <li>
+          <div class="navli" id="home-nav">
+
+          <a href="home.html">home</a>
+
+          </div>
+        </li>
+        <li>
+          <div class="navli" id="ramblings-nav">
+
+          <a href="ramblings.html">ramblings</a>
+
+          </div>
+        </li>
+        <li>
+          <div class="navli" id="tokipona-nav">
+
+          <a href="tokipona.html">toki pona</a>
+
+          </div>
+        </li>
+        <li>
+          <div class="navli" id="other-nav">
+
+          <a href="other.html">other</a>
+
+          </div>
+        </li>
+        <li>
+          <div class="navli" id="about-nav">
+
+          <a href="about.html">about</a>
+
+          </div>
+        </li>
+      </ul>
+
+    </nav>
+    `
+  }
+}
+
+customElements.define('header-part', HeaderPart);
---
+++ scripts/home.js
@@ -0,0 +1,97 @@
+var curpage = location.href.split("/").slice(-1);
+
+var pages = {
+  "home.html": document.getElementById('home-nav'),
+  "home.html#": document.getElementById('home-nav'),
+  "ramblings.html": document.getElementById('ramblings-nav'),
+  "ramblings.html#": document.getElementById('ramblings-nav'),
+  "tokipona.html": document.getElementById('tokipona-nav'),
+  "tokipona.html#": document.getElementById('tokipona-nav'),
+  "other.html": document.getElementById('other-nav'),
+  "other.html#": document.getElementById('other-nav'),
+  "about.html": document.getElementById('about-nav'),
+  "about.html#": document.getElementById('about-nav'),
+
+}
+
+var test = document.getElementsByClassName("navli");
+var highlight = document.getElementById('highlight');
+var navbar = document.getElementById('navbar');
+var container = document.getElementById('container');
+let travelspeed = 5;
+var widthspeed = 2;
+var highlightInterval;
+var idealPos, idealLength;
+var done = false;
+
+/*const onMouseMove = (e) =>{
+  idealPos = e.pageX;
+}*/
+
+//document.addEventListener('mousemove', onMouseMove);
+
+highlight.style.left = pages[curpage].getBoundingClientRect().left + window.scrollX + "px";
+highlight.style.width = pages[curpage].getBoundingClientRect().width + "px";
+
+window.onload = reload;
+window.onresize = reload;
+
+container.classList.add('fade');
+
+
+function reload(){
+  highlight.style.left = pages[curpage].getBoundingClientRect().left + window.scrollX + "px";
+  highlight.style.width = pages[curpage].getBoundingClientRect().width + "px";
+}
+
+
+
+for(var i = 0; i < test.length; i++){
+  test[i].addEventListener("mouseover", function(event){
+    clearInterval(highlightInterval);
+
+    idealPos = this.getBoundingClientRect().left + window.scrollX;
+    idealWidth = this.getBoundingClientRect().width;
+
+    moveHighlight();
+    highlightInterval = setInterval(moveHighlight, 1);
+  });
+
+}
+
+navbar.addEventListener('mouseout', function(){
+  clearInterval(highlightInterval);
+  idealPos = pages[curpage].getBoundingClientRect().left + window.scrollX;
+  idealWidth = pages[curpage].getBoundingClientRect().width;
+  moveHighlight();
+  highlightInterval = setInterval(moveHighlight, 1);
+});
+
+
+
+function moveHighlight() {
+  var lbStyle = window.getComputedStyle(highlight);
+  var leftValue = lbStyle.getPropertyValue("left").replace("px", "");
+  var widthValue = highlight.style.width.replace("px", "");
+
+  if(leftValue > idealPos - travelspeed && leftValue < idealPos + travelspeed){
+    highlight.style.left = idealPos + "px";
+    highlight.style.width = idealWidth + "px";
+    clearInterval(highlightInterval);
+    return;
+  }
+  else if(leftValue > idealPos){
+    highlight.style.left = (Number(leftValue) - travelspeed) + "px";
+  }
+  else if (leftValue < idealPos){
+    highlight.style.left = (Number(leftValue) + travelspeed) + "px";
+  }
+
+
+  if(widthValue > idealWidth){
+    highlight.style.width = (Number(widthValue) - widthspeed) + "px";
+  }
+  else if (widthValue < idealWidth){
+    highlight.style.width = (Number(widthValue) + widthspeed) + "px";
+  }
+}
